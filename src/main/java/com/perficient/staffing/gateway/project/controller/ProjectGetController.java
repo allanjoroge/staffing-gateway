8390dd3982bf1a9deb55104fba51cde7465afcd3
sabrahim10 <44107666+sabrahim10@users.noreply.github.com>
2020-02-03 14:53:01 -0600

    Added scanBasePackages because simple function would not display.

--- src/main/java/com/perficient/staffing/gateway/project/controller/ProjectGetController.java
+++ src/main/java/com/perficient/staffing/gateway/project/controller/ProjectGetController.java
@@ -3,21 +3,20 @@ package com.perficient.staffing.gateway.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
+
 public class ProjectGetController {
 
     @GetMapping("/projects")
     @ResponseStatus(HttpStatus.OK)
+    @Autowired
     public String getAllProjects(@RequestParam @Nullable Integer businessUnitId) {
-       return "business unit is "+ businessUnitId;
+       return "business unit is " + businessUnitId;
 
     }
 
--- src/main/java/com/perficient/staffing/staffinggateway/StaffingGatewayApplication.java
+++ src/main/java/com/perficient/staffing/staffinggateway/StaffingGatewayApplication.java
@@ -3,7 +3,7 @@ package com.perficient.staffing.staffinggateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.perficient.staffing") //scan base packages connects it to the local thing.
 public class StaffingGatewayApplication {
 
     public static void main(String[] args) {
