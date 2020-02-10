21db1639cc4b989149d45417c2965bb8c7d01821
Tracy Huber <51465294+megathuber@users.noreply.github.com>
2020-02-10 13:04:56 -0600

    added try catch to delete controller

--- src/main/java/com/perficient/staffing/gateway/project/controller/ProjectDeleteController.java
+++ src/main/java/com/perficient/staffing/gateway/project/controller/ProjectDeleteController.java
@@ -2,9 +2,10 @@ package com.perficient.staffing.gateway.project.controller;
 
 import com.perficient.staffing.gateway.project.service.ProjectDeleteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProjectDeleteController {
