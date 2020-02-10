3dd241b25fe922c2e30a0e80d96b45f338b0a328
sabrahim10 <44107666+sabrahim10@users.noreply.github.com>
2020-02-10 09:04:49 -0600

    ProjectDeleteServiceImpl addition

--- src/main/java/com/perficient/staffing/gateway/project/service/ProjectDeleteServiceImpl.java
+++ src/main/java/com/perficient/staffing/gateway/project/service/ProjectDeleteServiceImpl.java
@@ -1,10 +1,17 @@
 package com.perficient.staffing.gateway.project.service;
 
+import com.perficient.staffing.gateway.project.dto.ProjectDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class ProjectDeleteServiceImpl implements ProjectDeleteService {
 
     private RestTemplate restTemplate;
@@ -15,8 +22,16 @@ public class ProjectDeleteServiceImpl implements ProjectDeleteService {
     }
 
     @Override
+    @DeleteMapping("/projects/{id}")
     public boolean deleteByProjectId(Integer id) {
-        return false;
+        Optional<ProjectDTO> projectDTO = restTemplate.delete;
+
+        if (projectDTO.isPresent()) {
+            restTemplate.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
