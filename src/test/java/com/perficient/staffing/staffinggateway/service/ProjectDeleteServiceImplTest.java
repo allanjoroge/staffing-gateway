e5a0a77268996ae95defc4fef5f4c4d21d1299ef
andres.hunvonchong <andres.hunvonchong@perficient.com>
2020-02-11 10:19:05 -0600

    ProjectPostServiceImplTest

--- src/test/java/com/perficient/staffing/staffinggateway/service/ProjectDeleteServiceImplTest.java
+++ src/test/java/com/perficient/staffing/staffinggateway/service/ProjectDeleteServiceImplTest.java
@@ -2,23 +2,16 @@ package com.perficient.staffing.staffinggateway.service;
 
 import com.perficient.staffing.gateway.project.dto.ProjectDTO;
 import com.perficient.staffing.gateway.project.service.ProjectDeleteServiceImpl;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import static org.hamcrest.Matchers.is;
-
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProjectDeleteServiceImplTest {
