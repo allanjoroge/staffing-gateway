0db092408c345eaf9c3d6b34448a29aa694f49e6
allan.njoroge <A1ej4ndro!>
2020-02-20 13:44:41 -0600

    implemented feign client to use app-local.prop file for the URLs

--- src/main/java/com/perficient/staffing/gateway/project/client/ProjectClient.java
+++ src/main/java/com/perficient/staffing/gateway/project/client/ProjectClient.java
@@ -4,19 +4,17 @@ import com.perficient.staffing.gateway.project.dto.ProjectStatusDTO;
 import com.perficient.staffing.gateway.project.dto.ProjectTypeDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(url="http://localhost:8081", name = "staffing")
+@FeignClient(url= "${feign.client.url}", name = "staffing")
 public interface ProjectClient {
 
     @GetMapping("/project-types")
     //Created a method called getProjectTypes that returns a List of ProjectDTOs
     List<ProjectTypeDTO> getProjectTypes();
 
-    @RequestMapping(method = RequestMethod.GET, value = "/project-status")
+    @GetMapping("/project-status")
     List<ProjectStatusDTO> getProjectStatuses();
 
 
