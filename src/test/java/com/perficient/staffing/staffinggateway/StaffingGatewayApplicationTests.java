e18196bd6bc267709021da76a1f933d8ed5e90f8
sami.abrahim <sami.abrahim@perficient.com>
2020-02-26 14:36:01 -0600

    Added application-test.properties so that our context will pass the test.

--- src/test/java/com/perficient/staffing/staffinggateway/StaffingGatewayApplicationTests.java
+++ src/test/java/com/perficient/staffing/staffinggateway/StaffingGatewayApplicationTests.java
@@ -2,8 +2,10 @@ package com.perficient.staffing.staffinggateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StaffingGatewayApplicationTests {
 
     @Test
